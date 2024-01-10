6cc48cfef8d610057526fa4e0b65b2137912489c
misicaleksandar <aleksandarmishitj@gmail.com>
2024-01-10 15:10:24 +0100

    feat(domain): adding models and connection to db

---
+++ src/Domain/Models/DaysTally.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class DaysTally
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public DateTime Timestamp { get; set; }
+        [Required]
+        public string Action { get; set; }
+    }
+}
---
+++ src/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Leave> Leaves { get; set; }
+        public DbSet<DaysTally> DaysTallies { get; set; }
+        public DbSet<PublicHoliday> PublicHolidays { get; set; }
+
+    }
+}

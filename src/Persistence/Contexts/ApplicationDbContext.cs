03b2d249a5bfd00d6d325c979aad3a43486faecf
Aleksandar Mišić <80118002+misicaleksandar@users.noreply.github.com>
2024-01-14 12:15:35 +0100

    feat(identity): adding identity (#26)

--- src/Persistence/Contexts/ApplicationDbContext.cs
+++ src/Persistence/Contexts/ApplicationDbContext.cs
@@ -22,5 +22,21 @@ namespace Persistence.Contexts
         public DbSet<PublicHoliday> PublicHolidays { get; set; }
         public DbSet<Team> Teams { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Team>().HasData(
+                new Team() { Id = 1, Name = "Team1" },
+                new Team() { Id = 2, Name = "Team2" }
+                );
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Id = "3659f040-51a9-496a-958d-7030a439012e", Name = "TeamLead", NormalizedName = "TEAMLEAD" },
+                new IdentityRole { Id = "60024229-fb65-490d-abfd-f1369a56f952", Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = "6e875cce-3fc3-4ad3-87f5-9672ddeaa099", Name = "RegularEmployee", NormalizedName = "REGULAREMPLOYEE" });
+        }
+
     }
 }
---
+++ src/Services/Interfaces/Repositories/ITeamRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Services.Interfaces.Repositories
+{
+    public interface ITeamRepository : IRepository<Team>
+    {
+
+    }
+}

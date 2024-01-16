7626aa2ee1e197c85ab2a602efd85a1fcee20e95
misicaleksandar <aleksandarmishitj@gmail.com>
2024-01-16 07:57:10 +0100

    feat(identity): adding listing and editing employees

---
+++ src/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Persistence.Contexts;
+using Services.Interfaces.Repositories;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}

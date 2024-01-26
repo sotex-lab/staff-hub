448f5f00bf8c0f1d654fd81a500fb8b333f3f527
nikola20 <nikola.j00@gmail.com>
2024-01-26 16:23:47 +0100

    feat(email): implemented email sender

--- src/Services/CustomEmailSender.cs
+++ src/Services/CustomEmailSender.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Encodings.Web;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text.Encodings.Web;
 using Persistence.IRepository;
 
 namespace Services
@@ -15,7 +17,32 @@ namespace Services
             string subject = "Sotex Staff hub - Your account is created";
             string message = $"Please set your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
 
-            //here will be SendGrid logic
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential("nikola.j00@gmail.com", "GMAIL_PASS"),
+                    EnableSsl = true,
+                };
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("nikola.j00@gmail.com"),
+                    Subject = subject,
+                    Body = message,
+                    IsBodyHtml = true,
+                };
+
+                mailMessage.To.Add(email);
+
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending email: {ex.Message}");
+            }
+
 
             return Task.CompletedTask;
         }

7cd3159ecb9fd99d3a345385988551b1841ec0bd
Nikola Jovičić <jovicic.pr125.2019@uns.ac.rs>
2024-01-29 13:27:50 +0100

    fixes

--- src/Services/CustomEmailSender.cs
+++ src/Services/CustomEmailSender.cs
@@ -1,51 +1,48 @@
-﻿using System.Net;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using System.Text.Encodings.Web;
+using FluentEmail.Core;
+using FluentEmail.Smtp;
 using Persistence.IRepository;
 
 namespace Services
 {
     public class CustomEmailSender : ICustomEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25,
+            });
+
+            Email.DefaultSender = sender;
+
+            var sendingMail = await Email.From("EMAIL_FROM").To(email).Subject(subject).Body(htmlMessage).SendAsync();
         }
 
-        public Task SendResetPasswordTokenAsync(string email, string callbackUrl)
+        public async Task SendResetPasswordTokenAsync(string email, string callbackUrl)
         {
             string subject = "Sotex Staff hub - Your account is created";
             string message = $"Please set your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
 
-            try
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
             {
-                var smtpClient = new SmtpClient("smtp.gmail.com")
-                {
-                    Port = 587,
-                    Credentials = new NetworkCredential("nikola.j00@gmail.com", "GMAIL_PASS"),
-                    EnableSsl = true,
-                };
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25,
+            });
+            
+            Email.DefaultSender = sender;
 
-                var mailMessage = new MailMessage
-                {
-                    From = new MailAddress("nikola.j00@gmail.com"),
-                    Subject = subject,
-                    Body = message,
-                    IsBodyHtml = true,
-                };
-
-                mailMessage.To.Add(email);
-
-                smtpClient.Send(mailMessage);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error sending email: {ex.Message}");
-            }
-
-
-            return Task.CompletedTask;
+            var sendingMail = await Email
+                .From("EMAIL_FROM")
+                .To(email)
+                .Subject(subject)
+                .Body(message)
+                .SendAsync();
+            
         }
-
     }
 }
